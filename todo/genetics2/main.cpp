e55fdc0bcf73bcf27029657151ef2c524eacc571
Jacob Kloepper <kloepperwjacob@gmail.com>
2020-11-21 04:07:00 -0800

    adding genetics2

---
+++ todo/genetics2/main.cpp
@@ -0,0 +1,12 @@
+/* Jacob Kloepper
+ * November 21, 2020
+ * Kattis problem: genetics2
+ *  Link: https://open.kattis.com/problems/genetics2
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
