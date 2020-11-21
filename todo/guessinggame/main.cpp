979b557a54cf95b05d279aa9529239e1b48a8ff2
Jacob Kloepper <kloepperwjacob@gmail.com>
2020-11-21 00:36:56 -0800

    add guessinggame

---
+++ todo/guessinggame/main.cpp
@@ -0,0 +1,12 @@
+/* Jacob Kloepper
+ * November 21, 2020
+ * Kattis problem: guessinggame
+ *  Link: https://open.kattis.com/problems/guessinggame
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
