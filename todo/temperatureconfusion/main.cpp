ae7ae03844a7a6e0713174b4abb08d811aa3806e
Jacob Kloepper <kloepperwjacob@gmail.com>
2020-11-21 03:29:40 -0800

    add temperatureconfusion

---
+++ todo/temperatureconfusion/main.cpp
@@ -0,0 +1,12 @@
+/* Jacob Kloepper
+ * November 21, 2020
+ * Kattis problem: temperatureconfusion
+ *  Link: https://open.kattis.com/problems/temperatureconfusion
+ */
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!" << std::endl;
+	reutrn 0;
+}
